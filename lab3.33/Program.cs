8455802e73f169a44fea877f765b6c5bf94441a7
chirv <chirv@DESKTOP-LHI0AR3>
2021-11-23 18:56:05 +0200

    Добавьте файлы проекта.

---
+++ lab3.33/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите n");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите x");
+                double x = double.Parse(Console.ReadLine());
+                double S = 0;
+                {
+                    S += Math.Cos(2 * x) / 3 + Math.Cos(4 * x) / 15 + Math.Cos(6 * x) / 35;
+                }
+                Console.WriteLine("S={0:F2}", S);
+            }
+            catch
+            {
+            }
+
+        }
+    }
+}
+
